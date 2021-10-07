25075b9de44461526cc31a4070c081df560a562c
moeac <moecheaib@hotmail.com>
2021-10-07 09:07:05 +0800

    finalised all code with local storage and final css tweaks to make it somewhat useable

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// all the fun variables
 var questionsEl = document.getElementById("question");
 var answersEl = document.getElementById("answers");
 var answersContainer = document.getElementById("answers-container");
@@ -5,12 +6,14 @@ var qaContainer = document.querySelector(".qa-container");
 var gameOverDisplay = document.getElementById("game-over");
 var userID = document.getElementById("user-id");
 var scoreEl = document.getElementById("score");
+var currentScore = document.getElementById("current-score");
 var highscoreEl = document.getElementById("hs-list");
 var buttonStart = document.getElementById("start-button");
 var buttonRedo = document.getElementById("redo-button");
 var buttonClear = document.getElementById("clear-button");
 var buttonSubmit = document.getElementById("submit-button");
 var choiceEl = document.querySelectorAll(".choice");
+var viewScore = document.getElementById("high-score");
 var choiceA = document.getElementById("choiceA");
 var choiceB = document.getElementById("choiceB");
 var choiceC = document.getElementById("choiceC");
@@ -21,41 +24,41 @@ var message3 = document.getElementById("message-3");
 var timer = document.querySelector(".timer-count");
 var timerText = document.querySelector(".timer-text");
 var questionNumber = 0
-var timeLeft = 60;
+var timeLeft = 20;
 var timerInterval; 
 var score = 0;
 var highscoreList = [];
 
-
+// list of questions and answer options
 var question1 = {
-    question: 'QUESTION 1',
-    answers: ["answer1A", "answer1B", "answer1C", "answer1D"],
-    correctA: "answer1C",
+    question: 'Which of these is NOT a programming language?',
+    answers: ["Python", "JavaScript", "C#", "HTML"],
+    correctA: "HTML",
 
 };
 
 var question2 = {
-    question: 'QUESTION 2',
-    answers: ["answer2A", "answer1B", "answer1C", "answer1D"],
-    correctA: "answer1D",
+    question: 'Which of these is NOT an operator in JavaScript?',
+    answers: ["+", "-", "[ ]", "*"],
+    correctA: "[ ]",
 
 };
 
 var question3 = {
-    question: 'QUESTION 3',
-    answers: ["answer3A", "answer1B", "answer1C", "answer1D"],
-    correctA: "answer1C",
+    question: 'Which is the correct basic "for" loop iteration in JavaScript?',
+    answers: ["(i = 0; i < variable.length; i++)", "(i = 0; i > variable.length; i++)", "(i = 0; i < variable.length; i--)", "(i = 0; i > variable.length; i--)"],
+    correctA: "(i = 0; i < variable.length; i++)",
 
 };
 
 var question4 = {
-    question: 'QUESTION 4',
-    answers: ["answer4A", "answer1B", "answer1C", "answer1D"],
-    correctA: "answer1B",
+    question: 'Am I too lazy to write more questions?',
+    answers: ["Yes", "No", "Maybe", "The answer is Yes, I'm tired :("],
+    correctA: "Yes",
 
 };
 
-
+// array of above questions
 var questionList = [
     question1,
     question2,
@@ -63,24 +66,42 @@ var questionList = [
     question4,
 ];
 
+init();
 
-answersContainer.style.visibility = "hidden";
-gameOverDisplay.hidden = true;
 
-function clearScores() {
-    highscoreEl.textContent = "";
-    highscoreList = [];
+// gets high scores from local storage and runs on initialisation
+function init() {
+    var storedScoreList = JSON.parse(localStorage.getItem("highscores"));
+    if (storedScoreList !== null) {
+        highscoreList = storedScoreList;
+    }
+    displayHS();
 }
 
+
+// clears high scores from display and local storage
+function clearScores(event){
+    event.preventDefault();
+    highscoreEl.textContent = "";
+    localStorage.clear();
+    highscoreList = [];
+    
+}
+
+// displays highscores onto screen
+// resets highscoreEl each time so for loop doesnt repeat loop for previous scores
 function displayHS() {
     highscoreEl.textContent = "";
     for (var i = 0; i < highscoreList.length; i++) {
         var div = document.createElement("div");
-        div.textContent = highscoreList[i].name + " <-----> " + highscoreList[i].hScore;
+        div.classList.add("choice");
+        div.textContent = highscoreList[i].name + " got a score of " + highscoreList[i].hScore;
         highscoreEl.appendChild(div);
     }
+    
 }
 
+// saves score to local storage, hides submit button so user cant press twice and then runs displayHS() function
 function submitScore(event) {
     event.preventDefault();
     var initials = userID.value;
@@ -90,9 +111,11 @@ function submitScore(event) {
     };
     highscoreList.push(userHS);
     buttonSubmit.style.visibility ="hidden";
+    localStorage.setItem("highscores", JSON.stringify(highscoreList));
     displayHS();
 }
 
+// game over hides questions/answers and displays highscore board and stops timer
 function gameOver() {
     qaContainer.hidden = true;
     gameOverDisplay.hidden = false;
@@ -102,8 +125,9 @@ function gameOver() {
 
 }
 
+// activates timer, if time runs out, gameover(), if time less than 10sec, red text.
 function setTime() {
-    timeLeft = 60;
+    timeLeft = 20;
     timerInterval = setInterval(function() {
     timeLeft--;
     timer.textContent = timeLeft;
@@ -122,30 +146,37 @@ function setTime() {
 }, 1000);
 }
 
+// displays current score
 function scoreDisplay() {
-    scoreEl.textContent = "Current Score: " + score;
+    currentScore.textContent = "Current Score: " + score;
 }
 
+// hides message2 which is correct/wrong
 function hideMessage() {
     message2.textContent = " ";
     
 }
 
+// hides start button after clicking
 function hideButton() {
     buttonStart.style.display = "none";
 }
 
+// if current question# matches number of questions available, gameover.
 function displayQuestion() {
     if (questionNumber === questionList.length) {
         gameOver();
         return;
     }
+    // runs a loop to display current question/answers and displays the score
     questionsEl.textContent = questionList[questionNumber].question;
     for (var i = 0; i < questionList[questionNumber].answers.length; i++) {
         choiceEl[i].textContent = questionList[questionNumber].answers[i];
     }
+    scoreDisplay();
 }
 
+// checks if answer selected is correct and changes text color to red/green and reverts back to default colour after 350ms
 function correctAnswer(event) {
     event.preventDefault();
     
@@ -156,7 +187,7 @@ function correctAnswer(event) {
         message2.style.color = "green";
         questionNumber++;
         setTimeout(hideMessage, 350);
-        setTimeout(function(){selected.style.color = "black"}, 350);
+        setTimeout(function(){selected.style.color = "orange"}, 350);
         setTimeout(displayQuestion, 350);
         score++;
 
@@ -166,11 +197,12 @@ function correctAnswer(event) {
         message2.style.color = "red";
         questionNumber++;
         setTimeout(hideMessage, 350);
-        setTimeout(function(){selected.style.color = "black"}, 350);
+        setTimeout(function(){selected.style.color = "orange"}, 350);
         setTimeout(displayQuestion, 350);
     }
 }
 
+// resets values to default so user can repeat quiz
 function resetValues() {
     questionNumber = 0;
     score = 0;
@@ -181,7 +213,7 @@ function resetValues() {
     buttonSubmit.style.visibility = "visible"; 
 }
 
-
+// runs the functions
 function startQuestions() {
     resetValues();
     setTime();
@@ -189,14 +221,18 @@ function startQuestions() {
     displayQuestion();
 }
 
+// hides answers and gameover display until start is pressed
+answersContainer.style.visibility = "hidden";
+gameOverDisplay.hidden = true;
 
-
-
+// event listeners for the buttons
 buttonStart.addEventListener("click", startQuestions);
 buttonRedo.addEventListener("click", startQuestions);
 buttonSubmit.addEventListener("click", submitScore);
 buttonClear.addEventListener("click", clearScores);
+viewScore.addEventListener("click", gameOver);
 
+// event listeners for the answers clicked
 choiceA.addEventListener("click", correctAnswer);
 choiceB.addEventListener("click", correctAnswer);
 choiceC.addEventListener("click", correctAnswer);
